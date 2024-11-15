65fd750e969a8ea868a9d28309281916748eee73
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-11-15 14:58:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let fname = document.getElementById("fname")
+
+fname.addEventListener("blur", () => {
+	let temp = fname.value.toUpperCase();
+
+	fname.value = temp
+})
